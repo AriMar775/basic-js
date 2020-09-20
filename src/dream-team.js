35640da3601dfcdc430f784f5407b1c481f982ae
Arina Marchenko <arinamar7705@gmail.com>
2020-09-20 22:16:37 +0300

    feat: solved 'count-cats', 'carbon-dating' and 'dream-team' tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let result = [];
+  if (typeof members != "object" || members === null) return false;
+  else {
+    for (let i = 0; i <= members.length; i++) {
+      if (typeof members[i] === "string") {
+        result.push(members[i].trim().substring(0, 1).toUpperCase());
+      }
+    }
+  }
+  return result.sort().join("");
 };
