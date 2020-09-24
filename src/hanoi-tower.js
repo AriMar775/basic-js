24618d6cbc0b6e84b738b096a71440b685f1b41b
Arina Marchenko <arinamar7705@gmail.com>
2020-09-24 19:04:48 +0300

    fix: fix hanoi-tower script

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,5 @@
-module.exports = function calculateHanoi(disksNumber, turnsSpeed ) {
-  let turns = (2 ** disksNumber) - 1;
-  let seconds = Math.ceil(turns/(turnsSpeed/3600));
-  return {turns, seconds};
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let turns = 2 ** disksNumber - 1;
+  let seconds = Math.floor(turns / (turnsSpeed / 3600));
+  return { turns, seconds };
 };
