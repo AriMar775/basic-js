098f3722e39789b456bc258945118919c1c5bc96
Arina Marchenko <arinamar7705@gmail.com>
2020-09-25 19:13:58 +0300

    feat: add 'transform-array' task. need to solve

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr ) {
+  if(!arr) throw Error("Error");
+  let newArr = []
+  let aLength = arr.length;
+
+  for (let i = 0; i < aLength; i++){
+    if(i === 0 && (arr[i] === "--discard-next" 
+    || arr[i] === "--double-prev" 
+    || arr[i] === "--discard-prev")) 
+      continue;
+    if(arr[i] === "--discard-next" 
+    || arr[i-1] === "--discard-next" 
+    || arr[i+1] === "--double-prev" 
+    || arr[i] === "--discard-prev" 
+    || arr[i+1] === "--discard-prev"){
+      continue;
+    }
+    if(arr[i] === "--double-next"){
+      newArr.push(arr[i + 1]);
+      continue;
+    }
+    if(arr[i+1] === "--double-prev"){
+      newArr.push(arr[i]);continue;
+    }    
+      newArr.push(arr[i]);
+  }
+
+  return newArr;
 };
