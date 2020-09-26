a3d28536f4701bc49a449d4dc15dd881287c9769
Arina Marchenko <arinamar7705@gmail.com>
2020-09-26 14:58:18 +0300

    refactor: change transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,30 +1,26 @@
 
 module.exports = function transform(arr ) {
-  if(!arr) throw Error("Error");
+  if(!Array.isArray(arr)) throw Error("Error");
   let newArr = []
   let aLength = arr.length;
 
   for (let i = 0; i < aLength; i++){
-    if(i === 0 && (arr[i] === "--discard-next" 
-    || arr[i] === "--double-prev" 
-    || arr[i] === "--discard-prev")) 
+    if(i === 0 && (arr[i] === "--discard-prev" || arr[i] === "--double-prev" )) 
       continue;
     if(arr[i] === "--discard-next" 
     || arr[i-1] === "--discard-next" 
-    || arr[i+1] === "--double-prev" 
     || arr[i] === "--discard-prev" 
-    || arr[i+1] === "--discard-prev"){
-      continue;
-    }
+    || arr[i+1] === "--discard-prev")
+      continue;    
     if(arr[i] === "--double-next"){
       newArr.push(arr[i + 1]);
       continue;
     }
-    if(arr[i+1] === "--double-prev"){
-      newArr.push(arr[i]);continue;
-    }    
+    if(arr[i] === "--double-prev"){
+      newArr.push(arr[i-1]);continue;
+    } 
       newArr.push(arr[i]);
   }
 
-  return newArr;
+  return newArr.filter(item => item != undefined);
 };
