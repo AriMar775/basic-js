358909ef8714e1babc04d7cfcc5bb61ff13151ba
Arina Marchenko <arinamar7705@gmail.com>
2020-09-29 21:49:59 +0300

    feat: solved 'extended repeater' task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,29 +1,28 @@
 module.exports = function repeater(str, options) {
-  if(!str) throw Error("Error");
-  
   let result = "";
   let resultAddition = "";
   let separator = "+";
   let additionSeparator = "|";
 
-  if (options.separator) separator = options.separator.toString();
-  if (options.addition) resultAddition = options.addition.toString();
-  if (options.additionSeparator) additionSeparator = options.additionSeparator.toString();
+  if (options.separator) separator = String(options.separator);
+  if (options.addition != undefined || options.addition === null)
+    resultAddition = String(options.addition);
+  if (options.additionSeparator)
+    additionSeparator = String(options.additionSeparator);
 
-  if (options.additionRepeatTimes && options.additionRepeatTimes > 0) {
+  if (options.additionRepeatTimes) {
     for (let i = 1; i < options.additionRepeatTimes; i++) {
-      resultAddition +=  additionSeparator + options.addition;
+      resultAddition += additionSeparator + options.addition;
     }
   }
-  
-  result =  str + resultAddition;
 
-  if(options.repeatTimes && options.repeatTimes > 0) {
+  result = str + resultAddition;
+
+  if (options.repeatTimes && options.repeatTimes > 0) {
     for (let i = 1; i < options.repeatTimes; i++) {
-      result += separator + str + resultAddition ;
+      result += separator + str + resultAddition;
     }
   }
 
   return result;
 };
-  
