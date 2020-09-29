06ba86e807d4f406628bff53ef0cf726f1b88f49
Arina Marchenko <arinamar7705@gmail.com>
2020-09-29 23:16:02 +0300

    init: start 'vigenere cipher' task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,95 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(modification = true) {
+    this.modification = modification;
+  }
+
+  alphabetLength = 26;
+  asciiDiff = 97;
+
+  _normalizeKey(length, key) {
+    let result = key;
+    let index = 0;
+
+    for (let i = key.length; i < length; i++) {
+      if (index === key.length) {
+        index = 0;
+      }
+      result += key[index];
+      index++;
+    }
+
+    return result;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw Error("Error");
+
+    let result = "";
+    let normalizedKey = this._normalizeKey(
+      message.replace(" ", "").length,
+      key
+    );
+    let counter = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (!/[A-Za-z]/g.test(message[i])) {
+        result += message[i];
+        counter++;
+        continue;
+      }
+      result += String.fromCharCode(
+        ((message.charCodeAt(i) +
+          normalizedKey.charCodeAt(i - counter) -
+          this.asciiDiff * 2) %
+          this.alphabetLength) +
+          this.asciiDiff
+      );
+    }
+
+    return (this.modification
+      ? result
+      : result.split("").reverse().join("")
+    ).toUpperCase();
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw Error("Error");
+
+    let result = "";
+    let normalizedKey = this._normalizeKey(
+      message.replace(" ", "").length,
+      key
+    );
+    let counter = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (!/[A-Za-z]/g.test(message[i])) {
+        result += message[i];
+        counter++;
+        continue;
+      }
+      result += String.fromCharCode(
+        ((message.charCodeAt(i) +
+          this.alphabetLength -
+          normalizedKey.charCodeAt(i - counter) -
+          this.asciiDiff * 2) %
+          this.alphabetLength) +
+          this.asciiDiff
+      );
+    }
+
+    return (this.modification
+      ? result
+      : result.split("").reverse().join("")
+    ).toUpperCase();
   }
 }
 
 module.exports = VigenereCipheringMachine;
+// const directMachine = new VigenereCipheringMachine();
+// assert.equal(
+//   directMachine.encrypt("attack at dawn!", "alphonse"),
+//   "AEIHQX SX DLLU!"
+// );
+
+// console.log(directMachine.encrypt("attack at dawn!", "alphonse"));
