a09a029af80563ba6590497fd6816b792399fff4
Arina Marchenko <arinamar7705@gmail.com>
2020-09-29 13:22:53 +0300

    refactor: change extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,27 +1,26 @@
 module.exports = function repeater(str, options) {
   if(!str) throw Error("Error");
   let result = "";
-  let resultAddition ="";
+  let resultAddition = "";
   let separator = "+";
-  let addition = "";
   let additionSeparator = "|";
   if (options.separator) separator = options.separator.toString();
   if (options.addition) resultAddition = options.addition.toString();
   if (options.additionSeparator) additionSeparator = options.additionSeparator.toString();
 
   if (options.additionRepeatTimes && options.additionRepeatTimes > 0) {
-    for (let i = 0; i < options.additionRepeatTimes; i++) {
-      resultAddition += addition + additionSeparator;
+    for (let i = 1; i < options.additionRepeatTimes; i++) {
+      resultAddition +=  additionSeparator + options.addition;
     }
   }
-  else resultAddition += addition + additionSeparator;
+
+  result =  str + resultAddition;
 
   if(options.repeatTimes && options.repeatTimes > 0) {
-    for (let i = 0; i < options.repeatTimes; i++) {
-      result += str + resultAddition+ separator;
+    for (let i = 1; i < options.repeatTimes; i++) {
+      result += separator + str + resultAddition ;
     }
   }
-  else result +=  str + resultAddition+ separator;
 
   return result;
 };
